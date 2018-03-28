52b844cd4f4dc61c617623d677b17dcefbedd74d
Feather130 <1301788346@qq.com>
2018-03-28 12:42:58 +0800

    增加操作系统判断

--- index.js
+++ index.js
@@ -1,28 +1,56 @@
 let fs = require('fs')
 let path = require('path')
 let buffer = require('buffer')
-let fileName = []
-let fileAll = fs.readdirSync(__dirname)
-let buf = Buffer.alloc(500)
-let buf1 = Buffer.from('Feather')
-let buf2 = Buffer.from('feather150')
-for (let i = 0; i < fileAll.length; i++) {
-    let stat = fs.lstatSync(`${__dirname}\/${fileAll[i]}`)
-    if (stat.isDirectory()) {
-        fileName.push(fileAll[i])
-    }
+let os = require('os')
+
+function modify(search, replace, size) {
+	let fileName = []
+	let fileAll = fs.readdirSync(__dirname)
+	let buf = Buffer.alloc(size)
+	let buf1 = Buffer.from(search)
+	let buf2 = Buffer.from(replace)
+	let buf2Length = buf2.length
+	let stat, path
+
+	for (let i = 0; i < fileAll.length; i++) {
+		if (os === 'Windows_NT') {
+			stat = fs.lstatSync(`${__dirname}\\${fileAll[i]}`)
+		} else {
+			stat = fs.lstatSync(`${__dirname}\/${fileAll[i]}`)
+		}
+
+		if (stat.isDirectory()) {
+			fileName.push(fileAll[i])
+		}
+	}
+
+	for (let i = 0; i < fileName.length; i++) {
+
+		if (os === 'Windows_NT') {
+			path = `${__dirname}\\${fileName[i]}\\.git\\config`
+		} else {
+			path = `${__dirname}\/${fileName[i]}\/.git\/config`
+		}
+
+		fs.open(path, 'r+', (err, fd) => {
+			fs.read(fd, buf, 0, 500, 0, (err, bytesRead, buffer) => {
+				let position = buffer.indexOf(buf1)
+				if (position != -1) {
+					fs.write(fd, buf2, 0, buf2Length, position, (err, bytesRead, buffer) => {
+						if (err) {
+							throw err
+						} else {
+							console.log(`${fileName[i]}修改成功`)
+						}
+					})
+				} else {
+					console.log(`${fileName[i]}修改失败，没有找到内容`)
+				}
+			})
+		})
+	}
+
 }
-for (let i = 0; i < fileName.length; i++) {
-    fs.open(`${__dirname}\/${fileName[i]}\/.git\/config`, 'r+', (err, fd) => {
-        fs.read(fd, buf, 0, 500, 0, (err, bytesRead, buffer) => {
-            let position = buffer.indexOf(buf1)
-            fs.write(fd, buf2, 0, 10, position, (err, bytesRead, buffer) => {
-                if (err) {
-                    throw err
-                } else {
-                    console.log('修改成功')
-                }
-            })
-        })
-    })
-}
+
+
+modify('feather150', 'Feather130', 500)
